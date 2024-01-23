ccd056f0144e17d3cf7e3506bef34f7e488e639f
JonesHenrik <120439198+JonesHenrik@users.noreply.github.com>
2024-01-23 16:04:09 -0500

    Add files via upload

---
+++ Challenge4/Challenge4/Challenge4App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge4App.swift
+//  Challenge4
+//
+//  Created by Henrik Jones on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
